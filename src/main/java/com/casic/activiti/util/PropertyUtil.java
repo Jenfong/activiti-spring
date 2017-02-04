451addfc101d500e1c98b436e210650b691670fc
曹楚平 <cooge123@gmail.com>
2017-02-04 18:14:21 +0800

    新创建

---
+++ src/main/java/com/casic/activiti/util/PropertyUtil.java
@@ -0,0 +1,9 @@
+package com.casic.activiti.util;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.uuid.EthernetAddress;
+
+public class PropertyUtil extends PropertyPlaceholderConfigurer{
+
+}
